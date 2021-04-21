b0d98cdd1bf37d598b3fcf139c314119d0b548e3
OmerrKose <omer.kose@ibtech.com.tr>
2021-04-21 21:54:21 +0300

    Added button functionality

--- app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.ageinminutescalculator
 
+import android.app.DatePickerDialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener{view->
+            clickDatePicker(view)
+
+        }
+    }
+
+    private fun clickDatePicker(view: View){
+        val myCalendar = Calendar.getInstance()
+        val myYear = myCalendar.get(Calendar.YEAR)
+        val myMonth = myCalendar.get(Calendar.MONTH)
+        val myDayOfMonth = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(
+            this, { view, year, month, day ->
+                Toast.makeText(this, "Picker Works", Toast.LENGTH_LONG).show()
+            }
+            , myYear
+            , myMonth
+            , myDayOfMonth
+        ).show()
     }
 }
