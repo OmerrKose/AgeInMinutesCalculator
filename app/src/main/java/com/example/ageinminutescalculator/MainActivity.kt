d131508380913a95d156fb4997a70e30578afa64
OmerrKose <omer.kose@ibtech.com.tr>
2021-04-22 16:23:24 +0300

    Added full button functionality, optimization

--- app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.ageinminutescalculator
 
 import android.app.DatePickerDialog
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,25 +14,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnDatePicker.setOnClickListener{view->
+        btnDatePicker.setOnClickListener { view ->
             clickDatePicker(view)
-
         }
     }
 
-    private fun clickDatePicker(view: View){
+    private fun clickDatePicker(view: View) {
         val myCalendar = Calendar.getInstance()
         val myYear = myCalendar.get(Calendar.YEAR)
         val myMonth = myCalendar.get(Calendar.MONTH)
         val myDayOfMonth = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(
-            this, { view, year, month, day ->
-                Toast.makeText(this, "Picker Works", Toast.LENGTH_LONG).show()
-            }
-            , myYear
-            , myMonth
-            , myDayOfMonth
-        ).show()
+        val datePicker = DatePickerDialog(
+            this, { _, selectedYear, selectedMonth, selectedDay ->
+                Toast.makeText(this, "Date Picked", Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                val simpleDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                // divide by 60000 to convert date into minutes
+                val selectedDateInMinutes =
+                    (simpleDateFormat.parse(selectedDate))!!.time / 60000
+                val currentDateInMinutes =
+                    (simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis())))!!.time / 60000
+
+                // divide by 86400 to convert date into days
+                val selectedDateInDays =
+                    (simpleDateFormat.parse(selectedDate))!!.time / 86400000
+                val currentDateInDays =
+                    (simpleDateFormat.parse(simpleDateFormat.format(System.currentTimeMillis())))!!.time / 86400000
+
+                val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+                val differenceInDays = currentDateInDays - selectedDateInDays
+
+                tvSelectedDate.text = selectedDate
+                tvSelectedDateInMinutes.text = differenceInMinutes.toString()
+                tvSelectedDateInDays.text = differenceInDays.toString()
+
+            }, myYear, myMonth, myDayOfMonth
+        )
+
+        datePicker.datePicker.maxDate =
+            Date().time - 86400000 // don't display future time in the date picker
+        datePicker.show()
     }
 }
